4b194ca74569aa8cbcb1acf54fe3a1c9973ff012
harshp00 <harshkalppandya96@gmail.com>
2024-10-06 17:43:07 -0400

    added query engine and fav functionality

--- app.py
+++ app.py
@@ -9,10 +9,16 @@ from dotenv import load_dotenv
 
 sys.path.append(os.path.abspath('src'))
 
-from src.utils import list_catalog_schema_tables, create_erd_diagram, process_llm_response_for_mermaid, mermaid
-
-
+from src.utils import list_catalog_schema_tables, create_erd_diagram, process_llm_response_for_mermaid, mermaid,get_enriched_database_schema,create_sql, process_llm_response_for_sql, validate_and_correct_sql, load_data_from_query, create_advanced_sql
+from src.utils import add_to_user_history
 load_dotenv()
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="Text2SQL"
+
+
 
 
 st.set_page_config(
@@ -91,6 +97,81 @@ if authentication_status:
 
         
             
+            # Getting the table schema
+            table_schema = get_enriched_database_schema(catalog,schema,table_list)
+
+
+            # Deep-Dive Analysis
+        st.markdown("<h2 style='text-align: left; color: red;'> Deep-Dive Analysis </h2>", unsafe_allow_html=True)
+
+        with st.expander(":red[View the Section]"):
+            dd_question = st.text_area("Enter your question here..",key="dd-10",)
+
+            generate_sql_1 = st.checkbox("Generate SQL",key="dd-11")
+            if generate_sql_1:
+                response_sql_1 = create_sql(dd_question,table_schema)
+                response_sql_1 = process_llm_response_for_sql(response_sql_1)
+
+                # Self-correction loop
+                flag, response_sql_1 = validate_and_correct_sql(dd_question,response_sql_1,table_schema)
+                while flag != 'Correct':
+                    flag, response_sql_1 = validate_and_correct_sql(dd_question,response_sql_1,table_schema)
+
+                st.code(response_sql_1)
+
+                col1, col2 = st.columns(2)                
+
+                query_sample_data_1 = col1.checkbox("Query Sample Data",key="dd-12")
+                if query_sample_data_1:
+                    df_query_1 = load_data_from_query(response_sql_1)
+                    col1.write(df_query_1)
+
+
+                # Saving the Favourites    
+                # Adding session_state for favourite button
+                if 'fav_ind_1' not in st.session_state:
+                    st.session_state.fav_ind_1 = False
+
+                fav_ind_1 = col2.button("Save the query",key="dd-13")
+                if fav_ind_1:
+                    st.session_state.fav_ind_1 = True
+                    add_to_user_history(name,dd_question,response_sql_1,favourite_ind=True)
+                    col2.write("Added to favourites!")
+
+                build_1 = col1.checkbox("Build on top of this result?",key="dd-21")
+                if build_1:
+                    dd_question_2 = st.text_area("Enter your question here..",key="dd-23",)
+
+                    generate_sql_2 = st.checkbox("Generate SQL",key="dd-24")
+                    if generate_sql_2:
+                        response_sql_2 = create_advanced_sql(dd_question_2,response_sql_1,table_schema)
+                        response_sql_2 = process_llm_response_for_sql(response_sql_2)
+
+                        # Self-correction loop
+                        flag, response_sql_2 = validate_and_correct_sql(dd_question_2,response_sql_2,table_schema)
+                        while flag != 'Correct':
+                            flag, response_sql_2 = validate_and_correct_sql(dd_question_2,response_sql_2,table_schema)
+
+                        st.code(response_sql_2)
+
+                        col1, col2 = st.columns(2)
+                        query_sample_data_2 = col1.checkbox("Query Sample Data",key="dd-25")
+                        if query_sample_data_2:
+                            df_query_2 = load_data_from_query(response_sql_2)
+                            col1.write(df_query_2)
+
+
+                         # Saving the Favourites    
+                        # Adding session_state for favourite button
+                        if 'fav_ind_2' not in st.session_state:
+                            st.session_state.fav_ind_2 = False
+
+                        fav_ind_2 = col2.button("Save the query",key="dd3-13")
+                        if fav_ind_2:
+                            st.session_state.fav_ind_2 = True
+                            add_to_user_history(name,dd_question_2,response_sql_2,favourite_ind=True)
+                            col2.write("Added to favourites!")  
+                                                
 
 
 else:
@@ -99,12 +180,7 @@ else:
 
 
 
-# --> next step is to make sure we have all the context for the llm to work on 
-# --> generate the query
-# --> can have an extra add on query to the previous one (more in depth)  
-# --> validate the query if it is wrong
-# --> if it is wrong, rebuilt the query such that it works correctly
-# --> if it is right, then move ahead to further deeper analysis  
+
 
 # Instruction based (give set of instructions)
 # Role based promiting (act as cutomer service)
--- src/utils.py
+++ src/utils.py
@@ -160,3 +160,322 @@ def mermaid(code: str) -> None:
         """,
         height=800  # You can adjust the height as needed
     )
+
+
+
+
+@st.cache_data        
+def get_enriched_database_schema(catalog,schema,tables_list):
+
+    """
+    Function to create enriched database schema details for the Prompt
+    """
+
+    table_schema = ""
+
+    # Iterating through each selected tables and get the list of columns for each table.
+    for table in tables_list:
+
+        conn = sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                        http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
+                        access_token    = os.getenv("DATABRICKS_ACCESS_TOKEN"))        
+
+        # Getting the Schema for the table
+        query = f"SHOW CREATE TABLE `{catalog}`.{schema}.{table}"
+        df = pd.read_sql(sql=query,con=conn)
+        stmt = df['createtab_stmt'][0]
+        stmt = stmt.split("USING")[0]
+
+        # Filtering the String columns from the table to identify Categorical columns
+        query = f"DESCRIBE TABLE `{catalog}`.{schema}.{table}"
+        df = pd.read_sql(sql=query,con=conn)
+        string_cols = df[df['data_type']=='string']['col_name'].values.tolist()
+
+        sql_distinct = ""
+        for col in string_cols:
+            # Getting the distinct values for each column as rows
+            if col == string_cols[-1]:
+                sql_distinct += f"SELECT '{col}' AS column_name, COUNT(DISTINCT {col}) AS cnt, ARRAY_AGG(DISTINCT {col}) AS values FROM `{catalog}`.{schema}.{table}"
+            else:
+                sql_distinct += f"SELECT '{col}' AS column_name, COUNT(DISTINCT {col}) AS cnt, ARRAY_AGG(DISTINCT {col}) AS values FROM `{catalog}`.{schema}.{table} UNION ALL "
+
+        # print(sql_distinct)
+        df_categories = pd.read_sql(sql=sql_distinct,con=conn)
+        df_categories = df_categories[df_categories['cnt'] <= 20]
+        df_categories = df_categories.drop(columns='cnt')
+
+        if df_categories.empty:
+            df_categories_string = "No Categorical Fields"
+        else:
+            df_categories_string = df_categories.to_string(index=False)
+
+
+        # Getting the sample rows from the table
+        query = f"SELECT * FROM `{catalog}`.{schema}.{table} LIMIT 3"
+        df = pd.read_sql(sql=query,con=conn)
+        sample_rows = df.to_string(index=False)
+
+        if table_schema == "":
+            table_schema = stmt + "\n" + sample_rows + "\n\nCategorical Fields:\n" + df_categories_string + "\n"
+        else:
+            table_schema = table_schema + "\n" + stmt + "\n" + sample_rows + "\n\nCategorical Fields:\n" + df_categories_string + "\n"        
+    
+    return table_schema
+
+
+
+@st.experimental_fragment
+@st.cache_data
+def create_sql(question,table_schema):
+
+
+    """
+    Function to create SQL code for the selected question and return the data from the database
+  
+    """
+
+
+    ### Defining the prompt template
+    template_string = """ 
+    You are a expert data engineer working with a Databricks environment.\
+    Your task is to generate a working SQL query in Databricks SQL dialect. \
+    During join if column name are same please use alias ex llm.customer_id \
+    in select statement. It is also important to respect the type of columns: \
+    if a column is string, the value should be enclosed in quotes. \
+    If you are writing CTEs then include all the required columns. \
+    While concatenating a non string column, make sure cast the column to string. \
+    For date columns comparing to string , please cast the string input.\
+    For string columns, check if it is a categorical column and only use the appropriate values provided in the schema.\
+
+    SCHEMA:
+    ## {table_schema} ##
+
+    QUESTION:
+    ##
+    {question}
+    ##
+
+
+    IMPORTANT: MAKE SURE THE OUTPUT IS JUST THE SQL CODE AND NOTHING ELSE. Ensure the appropriate CATALOG is used in the query and SCHEMA is specified when reading the tables.
+    ##
+
+    OUTPUT:
+    """
+    prompt_template = PromptTemplate.from_template(template_string)
+
+    ### Defining the LLM chain
+    llm_chain = LLMChain(
+        llm=ChatOpenAI(model="gpt-4o-mini",temperature=0),
+        prompt=prompt_template
+    )
+
+    response =  llm_chain.invoke({"question":question,"table_schema":table_schema})
+    output = response['text']
+
+    return output
+
+
+
+def process_llm_response_for_sql(response: str) -> str:
+
+    """
+    Function to render the sql code
+
+    """
+    # Extract the Mermaid code block from the response
+    start_idx = response.find("```sql") + len("```sql")
+    end_idx = response.find("```", start_idx)
+    sql_code = response[start_idx:end_idx].strip()
+
+    return sql_code
+
+
+def validate_and_correct_sql(question,query,table_schema):
+
+    """ Final function to validate and self-correct
+    """
+    error_msg = self_correction(query)
+
+    if error_msg == "Successful":
+        # print("Query is successful")
+        return "Correct",query
+    else:
+        modified_query = correct_sql(question,query,table_schema,error_msg=error_msg)
+        return "Incorrect",modified_query
+    
+
+@st.experimental_fragment
+def self_correction(query):
+
+    """
+        Function to validate if self-correction is needed for the generated SQL query
+
+    """
+
+
+    error_msg = ""
+
+    try:
+        df = load_data_from_query(query)
+        # print(df.shape)
+        # df.head()
+        error_msg += "Successful"
+    except Exception as e:
+        error_msg += str(e)
+    
+    if error_msg == "Successful":
+        return error_msg
+    else:
+        # print("There is error")
+        # print(error_msg)
+        return error_msg
+    
+
+
+@st.experimental_fragment
+@st.cache_data
+def load_data_from_query(query):
+
+    """
+        Function to load data from the database given the SQL query
+
+    """
+
+
+
+    # Getting the sample details of the selected table
+    conn = sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                    http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
+                    access_token    = os.getenv("DATABRICKS_ACCESS_TOKEN"))
+
+    # query = query.replace(";","")
+    # query = query + f" LIMIT 1000;"
+    df = pd.read_sql(sql=query,con=conn)
+    return df        
+
+
+
+
+
+
+
+@st.experimental_fragment
+def correct_sql(question,sql_code,table_schema,error_msg):
+
+
+
+    """ 
+        Function to validate and self-correct generated SQL query    
+
+    """
+
+    ### Defining the prompt template
+    template_string = """ 
+    You are a expert data engineer working with a Databricks environment.\
+    Your task is to modify the SQL_CODE using Databricks SQL dialect based on the QUESTION, SCHEMA and the ERROR_MESSAGE. \
+    If ERROR_MESSAGE is provided, then make sure to correct the SQL query according to that. \
+
+    SCHEMA:
+    ## {table_schema} ##
+
+    ERROR_MESSAGE:
+    ## {error_msg} ##
+
+    SQL_CODE:
+    ##
+    {sql_code}
+
+    QUESTION:
+    ## {question} ##
+
+    ##
+
+
+    IMPORTANT: MAKE SURE THE OUTPUT IS JUST THE SQL CODE AND NOTHING ELSE. Ensure the appropriate CATALOG is used in the query and SCHEMA is specified when reading the tables.
+    ##
+
+    OUTPUT:
+    """
+    prompt_template = PromptTemplate.from_template(template_string)
+
+    ### Defining the LLM chain
+    llm_chain = LLMChain(
+        llm=ChatOpenAI(model="gpt-4o-mini",temperature=0),
+        prompt=prompt_template
+    )
+
+    response =  llm_chain.invoke({"question":question,"sql_code":sql_code,"table_schema":table_schema,"error_msg":error_msg})
+    output = response['text']
+
+    return output
+
+
+
+
+
+# Function to create SQL code for the selected question and return the data from the database
+@st.experimental_fragment
+@st.cache_data
+def create_advanced_sql(question,sql_code,table_schema):
+
+    ### Defining the prompt template
+    template_string = """ 
+    You are a expert data engineer working with a Databricks environment.\
+    Your task is to generate a working SQL query in Databricks SQL dialect. \
+    Enclose the complete SQL_CODE in a WITH clause and name it as MASTER. DON'T ALTER THE given SQL_CODE. \
+    Then based on the QUESTION and the master WITH clause, generate the final SQL query based on the WITH clause.\
+    ONLY IF additional information is needed to answer the QUESTION, then use the SCHEMA to join the details to get the final answer. \
+
+
+    INPUT:
+    SQL_CODE:
+    ##
+    {sql_code}
+    ##
+
+    SCHEMA:
+    ## {table_schema} ##
+
+    QUESTION:
+    ##
+    {question}
+    ##
+
+    IMPORTANT: MAKE SURE THE OUTPUT IS JUST THE SQL CODE AND NOTHING ELSE.
+    ##
+
+
+    OUTPUT:
+    """
+    prompt_template = PromptTemplate.from_template(template_string)
+
+    ### Defining the LLM chain
+    llm_chain = LLMChain(
+        llm=ChatOpenAI(model="gpt-4o-mini",temperature=0),
+        prompt=prompt_template
+    )
+
+    response =  llm_chain.invoke({"sql_code":sql_code,"question":question,"table_schema":table_schema})
+    output = response['text']
+
+    return output
+
+
+
+@st.experimental_fragment
+def add_to_user_history(user_name,question,query,favourite_ind):
+    
+    """
+        Add the selected question to the user history
+
+    """
+
+    conn = sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                    http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
+                    access_token    = os.getenv("DATABRICKS_ACCESS_TOKEN"))
+    
+    user_history_table = "hive_metastore.dev_tools.querysmart_user_query_history"
+
+    query = f"""INSERT INTO {user_history_table} VALUES ('{user_name}',current_timestamp(),'{question}',"{query}",{favourite_ind})"""
+    # query = f"SELECT * FROM {user_history_table}"
+    df = pd.read_sql(sql=query,con=conn)
